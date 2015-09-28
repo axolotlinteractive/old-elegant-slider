f02129b2fed92b5f9c9ef7b35d6cab820da0add0
Bryce Meyer <bryce@axolotlinteractive.com>
2015-09-27 20:28:31 -0500

    removed jquery override

--- slider.php
+++ slider.php
@@ -187,11 +187,7 @@ function elegant_slider_options_panel()
 
 function huge_it_slider_admin_script() {
     wp_enqueue_media();
-    wp_enqueue_style("jquery_ui", "http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css", FALSE);
-    if ( !defined( 'ICL_SITEPRESS_VERSION' ) || ICL_PLUGIN_INACTIVE ) {
-        wp_enqueue_script("jquery_new", "http://code.jquery.com/jquery-1.10.2.js", FALSE);
-        wp_enqueue_script("jquery_ui_new", "http://code.jquery.com/ui/1.10.4/jquery-ui.js", FALSE);
-    }
+    wp_enqueue_style("jquery_ui", "http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css", ['jquery']);
 
     wp_enqueue_script("simple_slider_js",  plugins_url("js/simple-slider.js", __FILE__), FALSE);
     wp_enqueue_style("simple_slider_css", plugins_url("style/simple-slider.css", __FILE__), FALSE);
